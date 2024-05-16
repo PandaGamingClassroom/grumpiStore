e304f307889324fd516aba26e6f6beeff4f8799d
OMENPC\User <Dani>
2024-05-16 10:15:27 +0200

    Update avatars selection

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
+import { AvatarService } from '../services/avatar/avatar.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,7 +11,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   avatar_list: any[] = [];
   avatar0 = '../../../assets/avatars/Avatar-1.png';
   avatar1 = '../../../assets/avatars/Avatar-2.png';
@@ -22,37 +23,28 @@ export class ProfileComponent {
   avatar7 = '../../../assets/avatars/Avatar-8.png';
   avatarSelect = '';
 
-  constructor() {
-    // Recuperar el avatar seleccionado desde sessionStorage
-   if (this.isBrowser()) {
-     const savedAvatar = sessionStorage.getItem('selectedAvatar');
-     if (savedAvatar) {
-       this.avatarSelect = savedAvatar;
-     } else {
-       this.avatarSelect =
-         'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp';
-     }
-   }
+  constructor(private avatarService: AvatarService) {
+    this.avatar_list = [
+      this.avatar0,
+      this.avatar1,
+      this.avatar2,
+      this.avatar3,
+      this.avatar4,
+      this.avatar5,
+      this.avatar6,
+      this.avatar7,
+    ];
+  }
 
-    this.avatar_list[0] = this.avatar0;
-    this.avatar_list[1] = this.avatar1;
-    this.avatar_list[2] = this.avatar2;
-    this.avatar_list[3] = this.avatar3;
-    this.avatar_list[4] = this.avatar4;
+  ngOnInit(): void {
+    this.avatarService.loadAvatarFromStorage();
+    this.avatarService.avatar$.subscribe((avatar) => {
+      this.avatarSelect = avatar;
+    });
   }
 
   avatarSelected(avatar: any) {
-    this.avatarSelect = avatar;
-    // Guardar el avatar seleccionado en sessionStorage
-    if (this.isBrowser()) {
-      sessionStorage.setItem('selectedAvatar', avatar);
-    }
+    this.avatarService.setAvatar(avatar);
   }
 
-  private isBrowser(): boolean {
-    return (
-      typeof window !== 'undefined' &&
-      typeof window.sessionStorage !== 'undefined'
-    );
-  }
 }
---
+++ src/app/components/services/avatar/avatar.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AvatarService {
+  private avatarSubject = new BehaviorSubject<string>(
+    'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp'
+  );
+  avatar$ = this.avatarSubject.asObservable();
+
+  setAvatar(avatar: string) {
+    this.avatarSubject.next(avatar);
+    localStorage.setItem('selectedAvatar', avatar);
+  }
+
+  loadAvatarFromStorage() {
+    const savedAvatar = localStorage.getItem('selectedAvatar');
+    if (savedAvatar) {
+      this.avatarSubject.next(savedAvatar);
+    }
+  }
+}
