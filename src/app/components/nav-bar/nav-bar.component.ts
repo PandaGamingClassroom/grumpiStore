3b050747e69e642670e0f9cbb5dd5b1b3d2cd982
OMENPC\User <Dani>
2024-05-16 10:15:27 +0200

    Update avatars selection

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AvatarService } from '../services/avatar/avatar.service';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
+  avatarSelect = '';
+  constructor(private avatarService: AvatarService) {}
+
+  ngOnInit(): void {
+    this.avatarService.avatar$.subscribe((avatar) => {
+      this.avatarSelect = avatar;
+    });
+  }
 }
