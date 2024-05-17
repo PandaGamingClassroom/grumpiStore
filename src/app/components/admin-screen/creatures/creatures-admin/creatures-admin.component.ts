c59c505c5021326a361ed7abf0f9925d7b61fdc8
OMENPC\User <Dani>
2024-05-17 15:31:09 +0200

    fix errors

--- src/app/components/admin-screen/creatures/creatures-admin/creatures-admin.component.ts
+++ src/app/components/admin-screen/creatures/creatures-admin/creatures-admin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GrumpiService } from '../../../services/grumpi/grumpi.service';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import {
   FormBuilder,
   FormGroup,
@@ -14,6 +14,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { CartaGrumpi, cartas_grumpi } from '../../../../models/grumpi';
 import { url_upload_grumpis } from '../../../../models/urls';
+import { ConfirmModalComponentComponent } from '../../../../segments/confirm-modal-component/confirm-modal-component.component';
 
 @Component({
   selector: 'app-creatures-admin',
@@ -37,11 +38,14 @@ export class CreaturesAdminComponent implements OnInit {
   selectedFile: File | null = null;
   imageUrls: string[] = [];
   uploadUrl: string = url_upload_grumpis;
+  modalAbierta = false;
+  confirmMessage: string = 'Grumpi añadido correctamente.';
 
   constructor(
     private grumpiService: GrumpiService,
     private formBuilder: FormBuilder,
-    private http: HttpClient
+    private http: HttpClient,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit() {
@@ -73,6 +77,7 @@ export class CreaturesAdminComponent implements OnInit {
     this.http.post(this.uploadUrl, formData).subscribe(
       (response) => {
         console.log('Imagen subida correctamente', response);
+        this.openModal();
       },
       (error: HttpErrorResponse) => {
         console.error('Error al enviar el grumpi', error);
@@ -91,5 +96,24 @@ export class CreaturesAdminComponent implements OnInit {
         console.error('Error al obtener las URLs de las imágenes:', error);
       }
     );
+  }
+
+  openModal() {
+    if (!this.modalAbierta) {
+      const data = {
+        title: '¡Grumpi guardado correctamente!',
+        message: this.confirmMessage,
+      };
+
+      const dialogRef = this.dialog.open(ConfirmModalComponentComponent, {
+        width: '400px',
+        height: '300px',
+        data: data,
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        window.location.reload();
+      });
+      this.modalAbierta = true;
+    }
   }
 }
--- src/app/segments/confirm-modal-component/confirm-modal-component.component.ts
+++ src/app/segments/confirm-modal-component/confirm-modal-component.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-modal-component',
   standalone: true,
   imports: [],
   templateUrl: './confirm-modal-component.component.html',
-  styleUrl: './confirm-modal-component.component.scss'
+  styleUrl: './confirm-modal-component.component.scss',
 })
 export class ConfirmModalComponentComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmModalComponentComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
+  closeDialog() {
+    this.dialogRef.close();
+    window.location.reload();
+  }
 }
