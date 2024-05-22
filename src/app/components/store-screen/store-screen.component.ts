196ba23fb9bacfa105d4ad2c2fc3ea91ab943a5f
OMENPC\User <Dani>
2024-05-22 14:34:26 +0200

    update bag cards

--- src/app/components/store-screen/store-screen.component.ts
+++ src/app/components/store-screen/store-screen.component.ts
@@ -8,6 +8,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { GrumpiService } from '../services/grumpi/grumpi.service';
+import { TrainerService } from '../services/trainers/trainer.service';
 
 @Component({
   selector: 'app-store-screen',
@@ -24,17 +25,28 @@ import { GrumpiService } from '../services/grumpi/grumpi.service';
     MedalsStoreComponent,
     FooterComponent,
   ],
-  providers: [GrumpiService],
+  providers: [GrumpiService, TrainerService],
   templateUrl: './store-screen.component.html',
   styleUrl: './store-screen.component.scss',
 })
 export class StoreScreenComponent implements OnInit {
   imageUrls: string[] = [];
+  grumpidolar: string = '';
+  trainer: any;
+  username: string | null = '';
 
-  constructor(private grumpiService: GrumpiService, private http: HttpClient) {}
+  constructor(
+    private grumpiService: GrumpiService,
+    private http: HttpClient,
+    private trainersService: TrainerService
+  ) {}
 
   ngOnInit(): void {
     this.loadImageUrls();
+    this.username = localStorage.getItem('username');
+    if (this.username) {
+      this.getTrainerData(this.username);
+    }
   }
 
   loadImageUrls() {
@@ -44,6 +56,26 @@ export class StoreScreenComponent implements OnInit {
       },
       (error) => {
         console.error('Error al obtener las URLs de las imágenes:', error);
+      }
+    );
+  }
+
+  /**
+   * Función para obtener información sobre el entrenador que ha iniciado sesión
+   * @param name recibe el nombre del entrenador
+   */
+  getTrainerData(name: string): void {
+    this.trainersService.getTrainerByName(name).subscribe(
+      (data) => {
+        if (data.message) {
+          console.log(data.message); // Maneja el mensaje de "Entrenador no encontrado"
+        } else {
+          this.trainer = data;
+          this.grumpidolar = this.trainer.data.grumpidolar;
+        }
+      },
+      (error) => {
+        console.error('Error:', error);
       }
     );
   }
