bd4d897e2e80391bede46a66c53e52a0d7c35688
OMENPC\User <Dani>
2024-05-22 12:09:43 +0200

    add footer section

--- src/app/components/help-component/help-component.component.ts
+++ src/app/components/help-component/help-component.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-help-component',
   standalone: true,
-  imports: [RouterLink, NavBarComponent],
+  imports: [RouterLink, NavBarComponent, FooterComponent],
   templateUrl: './help-component.component.html',
   styleUrl: './help-component.component.scss',
 })
