31582249817640d1128d4731710b53f8b2b5bcba
OMENPC\User <Dani>
2024-05-22 14:34:26 +0200

    update bag cards

--- src/app/components/profile/bag/bag/bag.component.ts
+++ src/app/components/profile/bag/bag/bag.component.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { TrainerService } from '../../../services/trainers/trainer.service';
+import { FooterComponent } from '../../../footer/footer.component';
 
 @Component({
   selector: 'app-bag',
@@ -20,6 +21,7 @@ import { TrainerService } from '../../../services/trainers/trainer.service';
     FormsModule,
     MatDialogModule,
     ReactiveFormsModule,
+    FooterComponent
   ],
   providers: [TrainerService],
   templateUrl: './bag.component.html',
