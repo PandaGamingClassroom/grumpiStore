d28d2ba934a210dabbce99f97023c5327c33e149
OMENPC\User <Dani>
2024-06-06 14:09:10 +0200

    update game

--- src/app/components/battle-game/battle-game.component.ts
+++ src/app/components/battle-game/battle-game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BattleService } from '../services/battle/battle.service';
 import { Attacks, Grumpi } from '../../models/grumpi';
 import { TrainerService } from '../services/trainers/trainer.service';
@@ -22,13 +22,15 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   providers: [GrumpiService, TrainerService],
   templateUrl: './battle-game.component.html',
-  styleUrl: './battle-game.component.scss',
+  styleUrls: ['./battle-game.component.scss'],
 })
-export class BattleGameComponent {
+export class BattleGameComponent implements OnInit {
   log: string[] = [];
   grumpiList: any;
   username: string | null = '';
   trainer: any;
+  selectedGrumpi: any | null = null;
+  randomGrumpi: any | null = null;
 
   constructor(
     private grumpiService: GrumpiService,
@@ -36,6 +38,7 @@ export class BattleGameComponent {
   ) {}
 
   ngOnInit() {
+    this.loadGrumpis();
     if (typeof window !== 'undefined') {
       // Verifica si `window` está definido
       this.username = localStorage.getItem('username');
@@ -63,6 +66,37 @@ export class BattleGameComponent {
     );
   }
 
+  loadGrumpis() {
+    this.grumpiService.getGrumpis().subscribe(
+      (response) => {
+        this.grumpiList = response.grumpis_list;
+        console.log('Lista de Grumpis: ', this.grumpiList);
+        this.randomGrumpi = this.getRandomGrumpi(); // Llamar a getRandomGrumpi después de cargar los Grumpis
+      },
+      (error) => {
+        console.error('Error al obtener las URLs de las imágenes:', error);
+      }
+    );
+  }
+
+  // Función para obtener un Grumpi aleatorio de la lista cargada
+  getRandomGrumpi() {
+    if (!this.grumpiList || this.grumpiList.length === 0) {
+      console.error(
+        'La lista de Grumpis está vacía. Asegúrese de haber llamado a loadGrumpis primero.'
+      );
+      return null;
+    }
+
+    const randomIndex = Math.floor(Math.random() * this.grumpiList.length);
+    const randomGrumpi = this.grumpiList[randomIndex];
+    console.log('Grumpi aleatorio seleccionado: ', randomGrumpi);
+    return randomGrumpi;
+  }
 
   attack(attacker: Grumpi, defender: Grumpi, move: Attacks) {}
+
+  selectCreature() {
+    console.log('Grumpi seleccionado: ', this.selectedGrumpi);
+  }
 }
