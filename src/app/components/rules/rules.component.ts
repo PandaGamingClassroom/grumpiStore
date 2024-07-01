5ee43dfbf8f280c81fc6e4b53158709e9763328b
OMENPC\User <Dani>
2024-07-01 14:03:39 +0200

    fix footer sizing

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-rules',
   standalone: true,
-  imports: [RouterLink, NavBarComponent],
+  imports: [RouterLink, NavBarComponent, FooterComponent],
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.scss'
 })
