34aaf9a65f8fdb487835ad6ae9d5bf3ea51b9595
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-09-11 12:04:13 +0200

    Añadidos los nuevos avatares de los alumnos

--- src/app/models/avatars.ts
+++ src/app/models/avatars.ts
@@ -18,34 +18,202 @@ export const lista_avatares: Avatars[] = [
   },
   {
     id: 4,
-    imagen: '../../../../assets/avatars/Avatar-1.png',
+    imagen: '../../../../assets/avatars/1.1.png',
   },
   {
     id: 5,
-    imagen: '../../../../assets/avatars/Avatar-2.png',
+    imagen: '../../../../assets/avatars/1.png',
   },
   {
     id: 6,
-    imagen: '../../../../assets/avatars/Avatar-3.png',
+    imagen: '../../../../assets/avatars/2.png',
   },
   {
     id: 7,
-    imagen: '../../../../assets/avatars/Avatar-4.png',
+    imagen: '../../../../assets/avatars/3.png',
   },
   {
     id: 8,
-    imagen: '../../../../assets/avatars/Avatar-5.png',
+    imagen: '../../../../assets/avatars/4.png',
   },
   {
     id: 9,
-    imagen: '../../../../assets/avatars/Avatar-6.png',
+    imagen: '../../../../assets/avatars/4.1.png',
   },
   {
     id: 10,
-    imagen: '../../../../assets/avatars/Avatar-7.png',
+    imagen: '../../../../assets/avatars/5.png',
   },
   {
     id: 11,
-    imagen: '../../../../assets/avatars/Avatar-8.png',
-  }
+    imagen: '../../../../assets/avatars/5.1.png',
+  },
+  {
+    id: 12,
+    imagen: '../../../../assets/avatars/6.png',
+  },
+  {
+    id: 13,
+    imagen: '../../../../assets/avatars/6.1.png',
+  },
+  {
+    id: 14,
+    imagen: '../../../../assets/avatars/7.png',
+  },
+  {
+    id: 15,
+    imagen: '../../../../assets/avatars/7.1.png',
+  },
+  {
+    id: 16,
+    imagen: '../../../../assets/avatars/8.png',
+  },
+  {
+    id: 17,
+    imagen: '../../../../assets/avatars/8.1.png',
+  },
+  {
+    id: 18,
+    imagen: '../../../../assets/avatars/9.png',
+  },
+  {
+    id: 19,
+    imagen: '../../../../assets/avatars/9.1.png',
+  },
+  {
+    id: 20,
+    imagen: '../../../../assets/avatars/10.png',
+  },
+  {
+    id: 21,
+    imagen: '../../../../assets/avatars/10.1.png',
+  },
+  {
+    id: 22,
+    imagen: '../../../../assets/avatars/11.png',
+  },
+  {
+    id: 23,
+    imagen: '../../../../assets/avatars/11.1.png',
+  },
+  {
+    id: 24,
+    imagen: '../../../../assets/avatars/12.png',
+  },
+  {
+    id: 25,
+    imagen: '../../../../assets/avatars/12.1.png',
+  },
+  {
+    id: 26,
+    imagen: '../../../../assets/avatars/13.png',
+  },
+  {
+    id: 27,
+    imagen: '../../../../assets/avatars/13.1.png',
+  },
+  {
+    id: 28,
+    imagen: '../../../../assets/avatars/14.png',
+  },
+  {
+    id: 29,
+    imagen: '../../../../assets/avatars/14.1.png',
+  },
+  {
+    id: 30,
+    imagen: '../../../../assets/avatars/15.png',
+  },
+  {
+    id: 31,
+    imagen: '../../../../assets/avatars/15.1.png',
+  },
+  {
+    id: 32,
+    imagen: '../../../../assets/avatars/16.png',
+  },
+  {
+    id: 33,
+    imagen: '../../../../assets/avatars/16.1.png',
+  },
+  {
+    id: 34,
+    imagen: '../../../../assets/avatars/17.png',
+  },
+  {
+    id: 35,
+    imagen: '../../../../assets/avatars/17.1.png',
+  },
+  {
+    id: 36,
+    imagen: '../../../../assets/avatars/18.png',
+  },
+  {
+    id: 37,
+    imagen: '../../../../assets/avatars/18.1.png',
+  },
+  {
+    id: 38,
+    imagen: '../../../../assets/avatars/19.png',
+  },
+  {
+    id: 39,
+    imagen: '../../../../assets/avatars/19.1.png',
+  },
+  {
+    id: 40,
+    imagen: '../../../../assets/avatars/20.png',
+  },
+  {
+    id: 41,
+    imagen: '../../../../assets/avatars/20.1.png',
+  },
+  {
+    id: 42,
+    imagen: '../../../../assets/avatars/21.png',
+  },
+  {
+    id: 43,
+    imagen: '../../../../assets/avatars/21.1.png',
+  },
+  {
+    id: 44,
+    imagen: '../../../../assets/avatars/22.png',
+  },
+  {
+    id: 45,
+    imagen: '../../../../assets/avatars/22.1.png',
+  },
+  {
+    id: 46,
+    imagen: '../../../../assets/avatars/23.png',
+  },
+  {
+    id: 47,
+    imagen: '../../../../assets/avatars/23.1.png',
+  },
+  {
+    id: 48,
+    imagen: '../../../../assets/avatars/24.png',
+  },
+  {
+    id: 49,
+    imagen: '../../../../assets/avatars/24.1.png',
+  },
+  {
+    id: 50,
+    imagen: '../../../../assets/avatars/25.png',
+  },
+  {
+    id: 51,
+    imagen: '../../../../assets/avatars/25.1.png',
+  },
+  {
+    id: 52,
+    imagen: '../../../../assets/avatars/26.png',
+  },
+  {
+    id: 53,
+    imagen: '../../../../assets/avatars/26.1.png',
+  }  
 ];
