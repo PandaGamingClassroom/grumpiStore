d16e035c65e4c21fe070edd6fde8a54b378fda93
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-09-13 10:20:38 +0200

    nueva funcion para urls

--- src/app/components/rules/career/career.component.ts
+++ src/app/components/rules/career/career.component.ts
@@ -10,32 +10,25 @@ import { NavBarAdminComponent } from '../../admin-screen/navBar-admin/nav-bar-ad
   templateUrl: './career.component.html',
   styleUrl: './career.component.scss',
 })
-export class CareerComponent implements OnInit{
+export class CareerComponent implements OnInit {
   showBackBTNHome: boolean = false;
   showBackBTNAdmin: boolean = false;
   
   constructor(private router: Router) { }
   
   ngOnInit(): void {
-    let currentUrl = this.router.url.split('?')[0];
-  
-    /**
-     *
-     * Se comprueba desde donde se accede a este componente
-     * para cambiar el botón que vuelve atrás.
-     *
-     */
+    let currentUrl = this.router.url.split('?')[0]; // Obtener la URL base sin los parámetros de consulta
     console.log('Ruta actual: ', currentUrl);
-    if (currentUrl === '/rules_admin') {
+
+    // Verificar si la URL comienza con una ruta específica
+    if (currentUrl.startsWith('/rules_admin')) {
       this.showBackBTNHome = false;
       this.showBackBTNAdmin = true;
-    } else if (currentUrl === '/rules') {
+    } else if (currentUrl.startsWith('/rules')) {
       this.showBackBTNHome = true;
       this.showBackBTNAdmin = false;
     }
   }
-  
-  
 
   disableRightClick(event: MouseEvent) {
     event.preventDefault();
