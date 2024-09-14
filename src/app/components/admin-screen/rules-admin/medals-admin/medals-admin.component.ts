dbcf75a4393a93f7d734babfea5080adc1185772
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-09-14 13:08:05 +0200

    Corrección barra de navegación en la guía de administrador

--- src/app/components/admin-screen/rules-admin/medals-admin/medals-admin.component.ts
+++ src/app/components/admin-screen/rules-admin/medals-admin/medals-admin.component.ts
@@ -6,8 +6,8 @@ import { NavBarAdminComponent } from '../../navBar-admin/nav-bar-admin/nav-bar-a
   selector: 'app-medals',
   standalone: true,
   imports: [RouterLink, NavBarAdminComponent],
-  templateUrl: './medals.component.html',
-  styleUrl: './medals.component.scss',
+  templateUrl: './medals-admin.component.html',
+  styleUrl: './medals-admin.component.scss',
 })
 export class MedalsAdminComponent {
   disableRightClick(event: MouseEvent) {
