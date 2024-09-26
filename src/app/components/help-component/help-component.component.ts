7c32a518fe43cee2933ed403d48e7a57bde185fb
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-09-26 20:28:52 +0200

    Nuevas funciones para ver correctamente las imágenes de ayuda

--- src/app/components/help-component/help-component.component.ts
+++ src/app/components/help-component/help-component.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { RouterLink } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { FooterComponent } from '../footer/footer.component';
+import { ViewImageComponent } from '../../segments/view-image/view-image.component';
 
 @Component({
   selector: 'app-help-component',
@@ -10,8 +12,62 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './help-component.component.html',
   styleUrl: './help-component.component.scss',
 })
-export class HelpComponentComponent {
+export class HelpComponentComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
   disableRightClick(event: MouseEvent) {
     event.preventDefault();
+  }
+  openImageWindow(selectedObject: string) {
+    let urlImage: string;
+    let titleImage: string;
+    if (selectedObject === 'TiposEnergia') {
+      urlImage = '../../../assets/carteleria/tiposEnergia.png';
+      titleImage = 'Tipos de energía';
+    } else if (selectedObject === 'ConseguirEnergias') {
+      urlImage = '../../../assets/carteleria/comoConseguirEnergia.png';
+      titleImage = 'Como conseguir energías';
+    } else if (selectedObject === 'Recompensas') {
+      urlImage = '../../../assets/carteleria/recompensa.png';
+      titleImage = 'Recompensas';
+    } else if (selectedObject === 'Grumpimonedas') {
+      urlImage = '../../../assets/carteleria/grumpimonedasMusica.pngg';
+      titleImage = 'Grumpimonedas Música';
+    } else if (selectedObject === 'GrumpimonedasEF') {
+      urlImage = '../../../assets/carteleria/grumpimonedasEF.png';
+      titleImage = 'Grumpimonedas EF';
+    } else if (selectedObject === 'instrucciones') {
+      urlImage =
+        '../../../assets/carteleria/Instrucciones de carta Grumpi.1.png.png';
+      titleImage = 'Instrucciones';
+    } else if (selectedObject === 'estadosAlterados') {
+      urlImage = '../../../assets/carteleria/estados_alterados.jpg';
+      titleImage = 'Estados alterados';
+    } else if (selectedObject === 'objCombate') {
+      urlImage = '../../../assets/carteleria/objCombate.png';
+      titleImage = 'Objetos de combate';
+    } else if (selectedObject === 'tienda') {
+      urlImage = '../../../assets/carteleria/TIENDA_GRUMPI.png.png';
+      titleImage = 'Tienda Grumpi';
+    } else if (selectedObject === 'tiendaObjCombate') {
+      urlImage = '../../../assets/carteleria/TIENDA_OBJETOSCOMBATE.png.png';
+      titleImage = 'Tienda Objetos de combate';
+    } else {
+      console.warn('Objeto no reconocido:', selectedObject);
+      return;
+    }
+
+    const data = {
+      title: titleImage,
+      message: urlImage,
+    };
+
+    this.dialog.open(ViewImageComponent, {
+      width: '600px',
+      height: '520px',
+      data: data,
+    });
   }
 }
