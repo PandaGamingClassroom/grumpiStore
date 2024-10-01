fdae423b008179b447f22a514b1e3bb6200f5f71
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-10-01 12:39:23 +0200

    Modificación para actualizaciones automáticas de la app instalada

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { inject } from '@angular/core';
 
 // Función para ocultar la pantalla de carga después de un tiempo
 function hideSplashScreen() {
@@ -13,10 +15,28 @@ function hideSplashScreen() {
   }
 }
 
+// Función para activar y verificar actualizaciones usando `versionUpdates`
+function checkForUpdates(swUpdate: SwUpdate) {
+  swUpdate.versionUpdates.subscribe((event) => {
+    if (event.type === 'VERSION_READY') {
+      if (confirm('Nueva versión disponible. ¿Actualizar ahora?')) {
+        window.location.reload();
+      }
+    }
+  });
+}
+
+// Bootstrap de la aplicación Angular
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient(), ...appConfig.providers],
 })
   .then(() => {
     hideSplashScreen();
+
+    // Comprobar actualizaciones si hay soporte para service workers
+    const swUpdate = inject(SwUpdate);
+    if (swUpdate.isEnabled) {
+      checkForUpdates(swUpdate);
+    }
   })
   .catch((err) => console.error(err));
