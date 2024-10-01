d4c716b2d658b42def99c360f469ce46b96376ec
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-10-01 12:24:59 +0200

    Nuevos logos y pantalla de carga

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 
+// Función para ocultar la pantalla de carga después de un tiempo
+function hideSplashScreen() {
+  const splashScreen = document.getElementById('splash-screen');
+  if (splashScreen) {
+    setTimeout(() => {
+      splashScreen.style.display = 'none';
+    }, 5000); // 2000 ms (2 segundos)
+  }
+}
+
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    ...appConfig.providers,
-  ],
-}).catch((err) => console.error(err));
+  providers: [provideHttpClient(), ...appConfig.providers],
+})
+  .then(() => {
+    hideSplashScreen();
+  })
+  .catch((err) => console.error(err));
