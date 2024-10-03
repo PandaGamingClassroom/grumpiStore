e1d261659ff9e56500a1a6732430143b7039d143
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-10-03 10:59:55 +0200

    UPDATE:  Ajuste para le navbar de admin

--- src/app/components/admin-screen/navBar-admin/nav-bar-admin/nav-bar-admin.component.ts
+++ src/app/components/admin-screen/navBar-admin/nav-bar-admin/nav-bar-admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar-admin',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-bar-admin.component.html',
   styleUrl: './nav-bar-admin.component.scss'
 })
