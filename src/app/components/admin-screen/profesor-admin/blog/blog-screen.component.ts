1a1f34ac6074c25bd1ded168e571bb90ea847e63
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-10-03 21:38:49 +0200

    UPDATE: Ajuste de la ventana del nuevo post

--- src/app/components/admin-screen/profesor-admin/blog/blog-screen.component.ts
+++ src/app/components/admin-screen/profesor-admin/blog/blog-screen.component.ts
@@ -3,34 +3,38 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule para ngModel
 
 @Component({
   selector: 'app-blog-screen',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, CommonModule],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule, CommonModule], // Asegúrate de importar FormsModule aquí
   templateUrl: './blog-screen.component.html',
   styleUrl: './blog-screen.component.scss',
 })
 export class BlogScreenComponent {
   postForm: FormGroup;
-  selectedFile: File | null = null;
+  selectedFiles: File[] = [];
   isTwoImages: boolean = false;
+  imageCount: string = 'una'; 
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.postForm = this.fb.group({
       title: ['', Validators.required],
       content: ['', Validators.required],
+      imageCount: ['una'] // Control para la cantidad de imágenes
     });
   }
 
-  onFileSelected(event: any) {
+  onImageCountChange(event: any) {
+    this.isTwoImages = event.target.value === 'dos';
+    this.selectedFiles = [];
+  }
+
+  onFileSelected(event: any, index: number) {
     const file: File = event.target.files[0];
-    const selectedType = event.target.value;
     if (file) {
-      this.selectedFile = file;
-    }
-    if (selectedType === "dos") {
-      this.isTwoImages = true;
+      this.selectedFiles[index] = file;
     }
   }
 
@@ -43,16 +47,20 @@ export class BlogScreenComponent {
       const formData = new FormData();
       formData.append('title', this.postForm.get('title')?.value);
       formData.append('content', this.postForm.get('content')?.value);
-      if (this.selectedFile) {
-        formData.append('image', this.selectedFile);
-        formData.append('image1', this.selectedFile);
-        formData.append('image2', this.selectedFile);
+
+      if (this.selectedFiles[0]) {
+        formData.append('image1', this.selectedFiles[0]);
+      }
+
+      if (this.isTwoImages && this.selectedFiles[1]) {
+        formData.append('image2', this.selectedFiles[1]);
       }
 
       this.http.post('YOUR_API_ENDPOINT/posts', formData).subscribe(
         (response) => {
           console.log('Post creado correctamente!', response);
           this.postForm.reset();
+          this.selectedFiles = [];
         },
         (error) => {
           console.error('Error creando el post', error);
