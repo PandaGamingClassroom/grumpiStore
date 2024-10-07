d96404b2486bc5fa20bf6283c34b2c175674fd2f
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-10-07 18:14:28 +0200

    NEW: Animaciones mensaje de bienvenida

--- src/app/segments/bienvenida-modal/bienvenida-modal.component.ts
+++ src/app/segments/bienvenida-modal/bienvenida-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-bienvenida-modal',
@@ -7,16 +9,28 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   imports: [],
   templateUrl: './bienvenida-modal.component.html',
   styleUrl: './bienvenida-modal.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        // Al entrar en la vista
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        // Al salir de la vista
+        animate('500ms ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class BienvenidaModalComponent implements OnInit {
-
   username: any;
   avatar = localStorage.getItem('selectedAvatar');
-  
+
   constructor(
     public dialogRef: MatDialogRef<BienvenidaModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     if (typeof window !== 'undefined') {
