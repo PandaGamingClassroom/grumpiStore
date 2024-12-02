51209ba3a79cb6b3ea6e93c1ea80f3f7475d24f1
PandaGamingClassroom <gamificacionpanda@gmail.com>
2024-12-02 18:05:34 +0100

    [FIX]: Arreglo guía grumpi

--- src/app/segments/view-image/view-image.component.ts
+++ src/app/segments/view-image/view-image.component.ts
@@ -12,7 +12,6 @@ import { RouterLink } from '@angular/router';
   selector: 'app-view-image',
   standalone: true,
   imports: [
-    RouterLink,
     CommonModule,
     FormsModule,
     MatDialogModule,
